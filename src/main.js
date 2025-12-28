3add8cf7cc6d213308e566fa83cb15e3de9963d1
kat <katwang7749@gmail.com>
2025-12-27 19:49:40 -0500

    it works

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import kaplay from "kaplay";
 import "kaplay/global"; // uncomment if you want to use without the  prefix
 
-kaplay();
+kaplay({
+    width: 1280,
+    height: 720,
+});
+
+
 
 loadRoot("./"); // A good idea for Itch.io publishing later
-loadSprite("bean", "sprites/bean.png");
+loadSprite("sixupgrade", "sprites/sixupgrade.png");
+loadSprite("annoyingkid", "sprites/annoyingkid.png");
 
 scene ("game", () => {
     var score = 0;
+    var numberOfSixUpgrades = 0;
+    var numberOfAnnoyingKidUpgrades = 0;
+
     const scoreText = add([
         text("", {
             size: 48,
@@ -18,15 +27,65 @@ scene ("game", () => {
         "scoreText"
     ])
     onUpdate(() => {
-        scoreText.text = score.toString();
+        scoreText.text = score.toFixed(4).replace(/\.?0+$/, "");
     })
     var scoreIncreaseAmount = 1;
     onClick(() => {
         score += scoreIncreaseAmount;
     })
-    add([
-        
+    const sixupgrade = add([
+        sprite("sixupgrade"),
+        pos(150, 120),
+        scale(0.2),
+        opacity(1),
+        area(),
+        anchor("center"),
+        "sixupgrade",
     ])
+    const annoyingkidupgrade = add([
+        sprite("annoyingkid"),
+        pos(430, 120),
+        scale(0.2),
+        opacity(1),
+        area(),
+        anchor("center"),
+        "annoyingkidupgrade",
+    ])
+
+    onClick("sixupgrade", () => {
+        if (score >= 6) {
+            score = score - 6;
+            numberOfSixUpgrades++;
+        }
+    })
+
+    onClick("annoyingkidupgrade", () => {
+        if (score >= 7) {
+            score = score - 7;
+            numberOfAnnoyingKidUpgrades++;
+        }
+    })
+
+    loop(1, () => {
+        score += numberOfSixUpgrades * 0.167;
+        score += numberOfAnnoyingKidUpgrades * 0.67;
+    })
+
+    onUpdate (() => {
+        // debug.log(score);
+        if (score < 6) {
+            sixupgrade.opacity = 0.5;
+        } else {
+            sixupgrade.opacity = 1;
+        }
+        if (score < 7) {
+            annoyingkidupgrade.opacity = 0.5;
+        } else {
+            annoyingkidupgrade.opacity = 1;
+        }
+    })
+
+
 })
 
 go("game");
